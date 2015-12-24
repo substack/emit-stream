2e56a6800252c42836a976c07799468304cc25a3
wouldgo <wouldgo84@gmail.com>
2015-12-24 16:38:55 +0100

    pointless commit

--- test/emit.js
+++ test/emit.js
@@ -7,10 +7,10 @@ var JSONStream = require('JSONStream');
 
 test('emit', function (t) {
     t.plan(1);
-    
+
     var server = (function () {
         var ev;
-        
+
         var server = net.createServer(function (stream) {
             if (!ev) ev = createEmitter();
             var s = JSONStream.stringify();
@@ -21,21 +21,21 @@ test('emit', function (t) {
         return server;
     })();
     server.listen(5555);
-    
+
     var collected = [];
-    
+
     server.on('listening', function () {
         var stream = net.connect(5555);
         var ev = emitStream(stream.pipe(JSONStream.parse([true])));
-        
-        ev.on('ping', function (t) {
+
+        ev.on('ping', function () {
             collected.push('ping');
         });
-        
+
         ev.on('x', function (x) {
             collected.push(x);
         });
-        
+
         setTimeout(function () {
             t.same(collected, [
                 0, 1, 2, 3, 'ping',
@@ -45,29 +45,29 @@ test('emit', function (t) {
             stream.end();
         }, 320);
     });
-    
+
     t.on('end', function () {
         server.close();
     });
 });
 
 function createEmitter () {
-    var ev = new EventEmitter;
+    var ev = new EventEmitter();
     var intervals = [];
     ev.stop = function () {
         intervals.forEach(function (iv) { clearInterval(iv) });
     };
-    
+
     setTimeout(function () {
         intervals.push(setInterval(function () {
             ev.emit('ping', Date.now());
         }, 100));
     }, 5);
-    
+
     var x = 0;
     intervals.push(setInterval(function () {
         ev.emit('x', x ++);
     }, 25));
-    
+
     return ev;
 }
